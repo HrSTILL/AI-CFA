380facb2e2a347f19746d45feea9aee3101217bf
HrSTILL <hristovstilian94@gmail.com>
2026-06-14 21:51:46 +0300

    More keywords added.

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ import csv
 def analyze_sentiment(text):
     text = text.lower()
 
-    if "bad" in text:
-        return "Negative"
-    elif "great" in text:
-        return "Positive"
-    else:
-        return "Neutral"
+    positive_words = [
+        "great",
+        "excellent",
+        "amazing",
+        "good",
+        "fast"
+    ]
+
+    negative_words = [
+        "bad",
+        "terrible",
+        "awful",
+        "slow",
+        "poor"
+    ]
+
+    for word in negative_words:
+        if word in text:
+            return "Negative"
+    for word in positive_words:
+        if word in text:
+            return "Positive"
+    return "Neutral"
 
 
 def count_sentiments(reviews):
@@ -35,12 +52,25 @@ with open("reviews.csv", "r") as file:
 
 stats = count_sentiments(reviews)
 
-with open("results.txt", "w") as file:
+with open("results.csv", "w", newline="") as file:
+    fieldnames = ["Review", "Sentiment"]
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    writer.writeheader()
+
     for review in reviews:
         result = analyze_sentiment(review)
-        file.write(review + " -> " + result + "\n")
 
-    file.write("\n")
+        writer.writerow({
+            "Review": review,
+            "Sentiment": result
+        })
+
+with open("summary.txt", "w") as file:
     file.write(f"Positive: {stats['Positive']}\n")
     file.write(f"Negative: {stats['Negative']}\n")
     file.write(f"Neutral: {stats['Neutral']}\n")
+
+print("Analysis completed.")
+print("Results saved to results.csv")
+print("Statistics saved to summary.txt")
